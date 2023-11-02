db0288d6f61d2ba2db6c150d2c59a09ad66edbb0
AzkaZharari <azka22060ti@student.nurulfikri.ac.id>
2023-11-02 20:35:31 +0700

    Praktikum Pertemuan 5

--- routes/api.php
+++ routes/api.php
@@ -33,4 +33,10 @@ Route::put('animals/{id}', [AnimalController::class, 'update']);
 Route::delete('animals/{id}', [AnimalController::class, 'destroy']);
 
 //membuat route student
-Route::get('students', [StudentController::class, 'index']);
+Route::get('/students', [StudentController::class, 'index']);
+
+Route::post('/students', [StudentController::class, 'store']);
+
+Route::put('/students/{id}', [StudentController::class, 'update']);
+
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
